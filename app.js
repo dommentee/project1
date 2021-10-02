faf8f4b76f9aa09430f5d43481cc6a958b7ea498
dommentee <chuku0929@gmail.com>
2021-10-02 17:48:12 -0400

    added files

---
+++ app.js
@@ -0,0 +1,21 @@
+// api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
+//(291.01K − 273.15) × 9/5 + 32
+console.log('hello');
+
+const key = '7d1e5512c847d1c72badc10dcb9d5e71'
+
+const cityName = 'Boston';
+const stateCode = 'ma';
+const countryCode = 'usa'
+
+$.ajax({
+  url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${countryCode}&appid=${key}`
+
+}).then(
+  (data) => {
+    console.log(data)
+  },
+  () => {
+    console.log('no data');
+  }
+)
