c694349149cb5e67458c17fb0d57fd50d62f184a
dommentee <chuku0929@gmail.com>
2021-10-03 17:03:34 -0400

    got values for each input city state country

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
 //(291.01K − 273.15) × 9/5 + 32
-console.log('hello');
 
 const key = '7d1e5512c847d1c72badc10dcb9d5e71'
 
@@ -8,20 +7,59 @@ let cityName = 'tokyo';
 let stateCode = '';
 let countryCode = 'jp'
 
-$.ajax({
-  url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${countryCode}&appid=${key}`
+const setStatperm = () => {
+  console.log('loading....');
+  $.ajax({
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${countryCode}&appid=${key}`
+  
+  }).then(
+    (data) => {
+      console.log(data)
+    },
+    () => {
+      console.log('no data');
+    }
+  )
+  
+} 
 
-}).then(
-  (data) => {
-    console.log(data)
-  },
-  () => {
-    console.log('no data');
-  }
-)
+// $.ajax({
+//   url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${countryCode}&appid=${key}`
+
+// }).then(
+//   (data) => {
+//     console.log(data)
+//   },
+//   () => {
+//     console.log('no data');
+//   }
+// )
 
 
 $(() => {
-
+  const $cityInput = $('#city-name').on('keyup', function () {
+    if ($(this).val()) {
+      cityName =  $(this).val()
+    } 
+  })
+  const $stateInput = $('#state-code').on('keyup', function () {
+    if ($(this).val()) {
+      stateCode =  $(this).val()
+    } 
+  })
+  const $countryInput = $('#country-code').on('keyup', function () {
+    if ($(this).val()) {
+      countryCode =  $(this).val()
+    } 
+  })
+  
+  $('form').on('submit', (e) => {
+    e.preventDefault();
+    console.log(cityName);
+    console.log(stateCode);
+    console.log(countryCode);
+  }) 
+  setStatperm ()
+  
 })
 
