61be40493486351599355bb3f0f6e7032cba0bfb
dommentee <chuku0929@gmail.com>
2021-10-04 17:43:16 -0400

    geo location and other api to for current loation

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 
 const key = '7d1e5512c847d1c72badc10dcb9d5e71'
 
-let cityName = 'tokyo';
+let cityName = '';
 let stateCode = '';
-let countryCode = 'jp'
+let countryCode = '';
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+const sucess = (pos) => {
+  let coordnates = pos.coords
+  let lat = coordnates.latitude
+  let long = coordnates.longitude
+  console.log('loading....');
+  console.log(coordnates);
+
+  $.ajax({//one 
+    url: `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`
+  }).then(
+    (data) => {
+
+      console.log(data)
+      // console.log(data.name);
+      
+    },
+    () => {
+      console.log('no data');
+    }
+  )
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(sucess, error, [options])
 
 const setStatperm = () => {// function that connect to the api
   console.log('loading....');
@@ -20,22 +54,6 @@ const setStatperm = () => {// function that connect to the api
   )
   
 }
-
-const getUserLocation = () => {
-
-}
-
-// $.ajax({
-//   url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${countryCode}&appid=${key}`
-
-// }).then(
-//   (data) => {
-//     console.log(data)
-//   },
-//   () => {
-//     console.log('no data');
-//   }
-// )
 
 
 $(() => {
